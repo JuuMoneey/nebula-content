a53cfdbe6f121bf5140c3484b0cbb42baf73ad63
Juumoneey <julietvorbe11@gmail.com>
2023-03-07 20:46:05 -0500

    finish

--- 1-variables/assignments/variable-hw.js
+++ 1-variables/assignments/variable-hw.js
@@ -4,6 +4,25 @@
 
 
 //1. Create a set of variables that define an animal (age, height, name, etc...). Must be at least 6 variables.
+const Name = 'Jaguar'
+const height = '7.5ft'
+const weight = '250 pounds'
+const location = 'South America'
+const furColor = 'tan with spots'
+const diet = 'carnivore'
 
+const animal = `${Name}, ${height}, ${weight}
+            ${location}, ${furColor}, ${diet}`
+console.log(animal)
 
 //2. Create a set of variables that define a vehicle. Must be at least 6 variables.
+const carName = Bentley Continental GT
+const cost = '200,000'
+const year = 2022
+const transmission = 8-speed Automatic
+const driveline = All Wheel drive
+const horsepower = 650 hp 
+
+const veichle = `${carName}, ${cost}, ${year},
+        ${transmission}, ${driveline}, ${horsepower}`
+console.log(veichle)
