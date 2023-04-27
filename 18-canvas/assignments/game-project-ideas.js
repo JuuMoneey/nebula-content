e04aa5dfec3bd2188c21eb8ccdbeef5ab1489983
Juumoneey <julietvorbe11@gmail.com>
2023-04-27 04:26:52 -0400

    Game Ideas

---
+++ 18-canvas/assignments/game-project-ideas.js
@@ -0,0 +1,25 @@
+// a point collections game
+// a enemy that eats all the coins like  dragon
+// power up to kill enemy 
+// object is to collect as much gems before you reach the finish line
+
+// maybe a racecar game
+// a car made out of candy
+// a speed boost where you collect candy when driving towards it 
+// wath out for candy monsters
+// sceneary options
+// car options
+// character options 
+// controls to move the car
+// maybe random weather changes
+// 
+
+// frogger kind of game 
+// sandwich crossig game 
+// a game with a character
+// object is to cross the road with a stacked sandwich
+// 5 lives max
+// sandwich gets wobbily and bigger as you cross 
+// energy bar if you drop the sandwich 
+// setting a timer
+// enemy would be birds maybe 
